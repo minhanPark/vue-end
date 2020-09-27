187ba8d973dba074bbd1712158351f44f7383749
Running water <alsgks1218@naver.com>
2020-09-27 23:44:37 +0900

    vue w-end

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -6,14 +6,23 @@
           <label for="username">id:</label>
           <input id="username" type="text" v-model="username" />
           <p class="validation-text">
-            <span class="warning" v-if="!isUsernameValid && username">Please enter an email address</span>
+            <span class="warning" v-if="!isUsernameValid && username">
+              Please enter an email address
+            </span>
           </p>
         </div>
         <div>
           <label for="password">pw:</label>
           <input id="password" type="text" v-model="password" />
         </div>
-        <button :disabled="!isUsernameValid || !password" type="submit" class="btn">로그인</button>
+        <button
+          :disabled="!isUsernameValid || !password"
+          type="submit"
+          class="btn"
+          :class="!isUsernameValid || !password ? 'disabled' : null"
+        >
+          로그인
+        </button>
       </form>
       <p class="log">{{ logMessage }}</p>
     </div>
